d684ee106023ee06e8c2724c8e36ae75e71561cf
Nikoloz Chachua <thenika600@gmail.com>
2026-09-20 14:10:26 +0400

    Refresh cache for restaurant favicons
    Updated cache name and added new favicon images.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bl-v217';
+const CACHE_NAME = 'bl-v218';
 
 const SUPABASE_URL  = 'https://lwdpegloznhpcecivhfy.supabase.co';
 const SUPABASE_ANON = 'sb_publishable_65dKKpb-lxOr8JTjdj7yxw_LZzcJp5h';
@@ -16,6 +16,8 @@ const PRECACHE = [
     './img/betareal-logo-light.png',
     './img/grain.png',
     './img/monday-greens-favicon.svg',
+    './img/food-and-market-favicon.png',
+    './img/corner-at-tabidze-favicon.png',
     './img/baoma/interior-hero-maps.webp',
     './img/baoma/interior-terrace.jpg',
     './assets/mugsy/logo.svg',
